3af845dd285746d8187976304c851fa8b358b223
tanvirch0750 <tanvir0750@yahoo.com>
2021-10-30 12:44:32 +0600

    Get serverSideProps & getStaticProps

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,30 @@
 import Layout from "@/components/Layout";
+import { API_URL } from "@/config/index";
 
-export default function HomePage() {
+export default function HomePage({ events }) {
+  console.log(events);
   return (
     <Layout>
       <h1>Home Page</h1>
     </Layout>
   );
+}
+
+// export async function getServerSideProps() {
+//   const res = await fetch(`${API_URL}/api/events`);
+//   const events = await res.json();
+
+//   return {
+//     props: { events },
+//   };
+// }
+
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+
+  return {
+    props: { events },
+    revalidate: 1,
+  };
 }
