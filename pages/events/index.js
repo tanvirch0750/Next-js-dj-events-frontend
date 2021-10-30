687539e7fe1265e27d4949640c257a8acff7aae4
tanvirch0750 <tanvir0750@yahoo.com>
2021-10-30 14:17:18 +0600

    Displaying events

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,9 +1,26 @@
 import Layout from "@/components/Layout";
+import { API_URL } from "@/config/index";
+import EventItem from "@/components/EventItem";
 
-export default function EventsPage() {
+export default function EventsPage({ events }) {
+  console.log(events);
   return (
-    <Layout title="All Events | Dj Events">
-      <h1>My Events</h1>
+    <Layout title="All Events">
+      <h1>Find your desired events</h1>
+      {events.length === 0 && <h3>No events to show</h3>}
+      {events.map((evt) => (
+        <EventItem evt={evt} key={evt.id} />
+      ))}
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+
+  return {
+    props: { events },
+    revalidate: 1,
+  };
 }
--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,32 @@
 import Layout from "@/components/Layout";
 import { API_URL } from "@/config/index";
+import EventItem from "@/components/EventItem";
+import Link from "next/link";
 
 export default function HomePage({ events }) {
   console.log(events);
   return (
     <Layout>
       <h1>Home Page</h1>
+      {events.length === 0 && <h3>No events to show</h3>}
+      {events.map((evt) => (
+        <EventItem evt={evt} key={evt.id} />
+      ))}
+      {events.length > 0 && (
+        <Link href="/events">
+          <a className="btn-secondary">View All Events</a>
+        </Link>
+      )}
     </Layout>
   );
 }
-
-// export async function getServerSideProps() {
-//   const res = await fetch(`${API_URL}/api/events`);
-//   const events = await res.json();
-
-//   return {
-//     props: { events },
-//   };
-// }
 
 export async function getStaticProps() {
   const res = await fetch(`${API_URL}/api/events`);
   const events = await res.json();
 
   return {
-    props: { events },
+    props: { events: events.slice(0, 3) },
     revalidate: 1,
   };
 }
